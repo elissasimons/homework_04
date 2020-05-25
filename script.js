1d307fabaa5696cdcb7c98331661960487f5a308
Elissa Simons <elissasimons@gmail.com>
2020-05-25 16:49:32 -0700

    created containers for different sections of quiz

---
+++ script.js
@@ -0,0 +1,9 @@
+var startButton = document.getElementById('startBtn');
+//create onclick function
+//console log when button clicks
+
+
+//create arrays for questions, answers etc
+//for loop to run through
+
+//create array for answers
