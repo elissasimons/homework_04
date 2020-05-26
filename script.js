d1e25b416c2b49bee786663258906a273826c193
Elissa Simons <elissasimons@gmail.com>
2020-05-25 21:54:46 -0700

    added buttons and style, also started to create functions

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-var startButton = document.getElementById('startBtn');
+var startButton = document.getElementById('start-btn');
+var questionContainerElement = document.getElementById('quizContainer');
+var shuffleQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame);
+
 //create onclick function
 //console log when button clicks
 
@@ -7,3 +12,28 @@ var startButton = document.getElementById('startBtn');
 //for loop to run through
 
 //create array for answers
+
+function startGame() {
+    console.log('Game has started');
+    startButton.classList.add('hide');
+    shuffleQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    showQuestion(shuffleQuestions[currentQuestionIndex]);
+}
+
+function selectAnswer() {
+
+}
+
+var questions = [
+    //question: 'Question 1 is here',
+    //answers: [
+       // { text: '4', correct: true},
+      //  { text: '22', correct: false }
+    //]
+]
