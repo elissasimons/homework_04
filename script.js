b9b8769a13b729f97e383caa0864ba3b7bac06ba
Elissa Simons <elissasimons@gmail.com>
2020-05-27 23:22:28 -0700

    changed css style, added function

--- script.js
+++ script.js
@@ -1,39 +1,107 @@
-var startButton = document.getElementById('start-btn');
-var questionContainerElement = document.getElementById('quizContainer');
-var shuffleQuestions, currentQuestionIndex
+var startButton = document.getElementById('start-btn')
+var nextButton = document.getElementById('next-btn')
+var questionContainerElement = document.getElementById('quiz-container')
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-buttons')
 
-startButton.addEventListener('click', startGame);
+let shuffledQuestions, currentQuestionIndex
 
-//create onclick function
-//console log when button clicks
-
-
-//create arrays for questions, answers etc
-//for loop to run through
-
-//create array for answers
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
 function startGame() {
-    console.log('Game has started');
-    startButton.classList.add('hide');
-    shuffleQuestions = questions.sort(() => Math.random() - .5);
-    currentQuestionIndex = 0;
-    questionContainerElement.classList.remove('hide');
-    setNextQuestion()
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
 function setNextQuestion() {
-    showQuestion(shuffleQuestions[currentQuestionIndex]);
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function selectAnswer() {
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    var button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
 
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
 
 var questions = [
-    //question: 'Question 1 is here',
-    //answers: [
-       // { text: '4', correct: true},
-      //  { text: '22', correct: false }
-    //]
+  {
+    question: "What is a JavaScript Data Type?",
+    answers: [
+      { text: "Object", correct: true },
+      { text: "Array", correct: false },
+      { text: "Variable", correct: false },
+      { text: "Tag", correct: false },
+    ]
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+      { text: '<js>', correct: false },
+      { text: '<script>', correct: true },
+      { text: '<javascript>', correct: false },
+      { text: '<style>', correct: false },
+    ]
+  },
+  {
+    question: 'Who invented JavaScript?',
+    answers: [
+      { text: 'Douglas Crockford', correct: false },
+      { text: 'Sheryl Sandberg', correct: false },
+      { text: 'Brendan Eich', correct: true },
+      { text: 'Jeff Bridges', correct: false },
+    ]
+  }
 ]
