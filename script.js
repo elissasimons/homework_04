c015143b5643b4caf5d645cea193498ab248f66c
Elissa Simons <elissasimons@gmail.com>
2020-05-28 00:48:42 -0700

    changed restart button name

--- script.js
+++ script.js
@@ -3,104 +3,137 @@ var nextButton = document.getElementById('next-btn')
 var questionContainerElement = document.getElementById('quiz-container')
 var questionElement = document.getElementById('question')
 var answerButtonsElement = document.getElementById('answer-buttons')
-var shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex
+
 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
+    currentQuestionIndex++
+    setNextQuestion()
 })
 
 function startGame() {
-  startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    var button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
-      button.dataset.correct = answer.correct
-    }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 }
 
 function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
-  while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-  }
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
 }
 
 function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
-  } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
-  }
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Quiz Is Over! Restart'
+        startButton.classList.remove('hide')
+    }
 }
 
 function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add('correct')
-  } else {
-    element.classList.add('wrong')
-  }
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
 }
 
 function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
-var questions = [
-  {
-    question: "What is a JavaScript Data Type?",
-    answers: [
-      { text: "Object", correct: true },
-      { text: "Array", correct: false },
-      { text: "Variable", correct: false },
-      { text: "Tag", correct: false },
-    ]
-  },
-  {
-    question: "Inside which HTML element do we put the JavaScript?",
-    answers: [
-      { text: '<js>', correct: false },
-      { text: '<script>', correct: true },
-      { text: '<javascript>', correct: false },
-      { text: '<style>', correct: false },
-    ]
-  },
-  {
-    question: 'Who invented JavaScript?',
-    answers: [
-      { text: 'Douglas Crockford', correct: false },
-      { text: 'Sheryl Sandberg', correct: false },
-      { text: 'Brendan Eich', correct: true },
-      { text: 'Jeff Bridges', correct: false },
-    ]
-  }
+var questions = [{
+        question: "What is a JavaScript Data Type?",
+        answers: [{
+                text: "Object",
+                correct: true
+            },
+            {
+                text: "Array",
+                correct: false
+            },
+            {
+                text: "Variable",
+                correct: false
+            },
+            {
+                text: "Tag",
+                correct: false
+            },
+        ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [{
+                text: '<js>',
+                correct: false
+            },
+            {
+                text: '<script>',
+                correct: true
+            },
+            {
+                text: '<javascript>',
+                correct: false
+            },
+            {
+                text: '<style>',
+                correct: false
+            },
+        ]
+    },
+    {
+        question: 'Who invented JavaScript?',
+        answers: [{
+                text: 'Douglas Crockford',
+                correct: false
+            },
+            {
+                text: 'Sheryl Sandberg',
+                correct: false
+            },
+            {
+                text: 'Brendan Eich',
+                correct: true
+            },
+            {
+                text: 'Jeff Bridges',
+                correct: false
+            },
+        ]
+    }
 ]
